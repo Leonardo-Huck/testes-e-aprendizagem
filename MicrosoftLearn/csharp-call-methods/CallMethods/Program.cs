3338c0040aafdde03d459f3de44bd2b7d3d3f26b
Leonardo <leonardomonteirolima@gmail.com>
2024-02-19 23:06:32 -0300

    Adicionado as pastas dos cursos

---
+++ MicrosoftLearn/csharp-call-methods/CallMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Random dice = new Random();
+        int roll = dice.Next(1, 20);
+        Console.WriteLine(roll);
+
+        // versões sobrecarregadas do metodo WriteLine() 
+        int number = 7;
+        string text = "seven";
+        //O primeiro método WriteLine() usa uma assinatura de método que define um parâmetro int
+        Console.WriteLine(number);
+        // O segundo método WriteLine() usa uma assinatura de método que define zero parâmetros de entrada
+        Console.WriteLine();
+        // O terceiro método WriteLine() usa uma assinatura de método que define um parâmetro string.
+        Console.WriteLine(text);
+
+        Random dice2 = new Random();
+        int roll1 = dice2.Next(); // retorna 0 a 2,147,483,647 que é o limite de int
+        int roll2 = dice2.Next(101); // 0 e 100
+        int roll3 = dice2.Next(50, 101); //  50 e 100
+
+        Console.WriteLine($"First roll: {roll1}");
+        Console.WriteLine($"Second roll: {roll2}");
+        Console.WriteLine($"Third roll: {roll3}");
+
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue = Math.Max(firstValue, secondValue); //sobrecarga de inteiros.
+
+        Console.WriteLine(largerValue);
+    }
+}
---
+++ MicrosoftLearn/csharp-if-elseif-else/Program.cs
@@ -0,0 +1,81 @@
+﻿// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples! +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//         total += 2;
+//     }
+//     Console.WriteLine($"Your total including the bonus: {total}");
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (total >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (total == 7)
+// {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("You win a kitten!");
+// }
+
+// Desafio
+
+using System.Formats.Asn1;
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if ((daysUntilExpiration <= 5) && (daysUntilExpiration > 0))
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
