eec6a4fceca9f038304c482ee9f135abba6e5016
Leonardo <leonardomonteirolima@gmail.com>
2024-02-21 19:21:51 -0300

    Acessando os valores da array

--- MicrosoftLearn/csharp-arrays/Program.cs
+++ MicrosoftLearn/csharp-arrays/Program.cs
@@ -3,3 +3,7 @@
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
