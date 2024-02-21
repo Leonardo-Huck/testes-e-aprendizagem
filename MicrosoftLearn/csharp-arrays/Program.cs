7076d7277e737d52232c1babbc1a27ecc446b455
Leonardo <leonardomonteirolima@gmail.com>
2024-02-21 19:25:24 -0300

    Atribuindo valores na declaração da variavél

--- MicrosoftLearn/csharp-arrays/Program.cs
+++ MicrosoftLearn/csharp-arrays/Program.cs
@@ -1,9 +1,16 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿/* string[] fraudulentOrderIDs = new string[3];
 
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
+*/
+
+string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
 
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
