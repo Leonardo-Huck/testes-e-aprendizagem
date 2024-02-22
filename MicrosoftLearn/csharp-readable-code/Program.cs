491613b436cab0b32be81cf24342fb88c32a997f
Leonardo <leonardomonteirolima@gmail.com>
2024-02-22 19:45:25 -0300

    Removido todos os comentarios desnecessarios do codigo

--- MicrosoftLearn/csharp-readable-code/Program.cs
+++ MicrosoftLearn/csharp-readable-code/Program.cs
@@ -9,16 +9,12 @@ string[] orderIDs = new string[5];
 
 for (int i = 0; i < orderIDs.Length; i++)
 {
-    // Get a random value that equates to ASCII letters A through E
     int prefixValue = random.Next(65, 70);
-    // Convert the random value into a char, then a string
     string prefix = Convert.ToChar(prefixValue).ToString();
-    // Create a random number, pad with zeroes
     string suffix = random.Next(1, 1000).ToString("000");
-    // Combine the prefix and suffix together, then assign to current OrderID
     orderIDs[i] = prefix + suffix;
 }
-//Print out each order ID
+
 foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
