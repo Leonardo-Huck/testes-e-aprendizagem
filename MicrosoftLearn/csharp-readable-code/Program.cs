b00f0f0221109a5e82cf9d304b9ff9c22a511628
Leonardo <leonardomonteirolima@gmail.com>
2024-02-22 19:42:45 -0300

    Criado um codigo que gera numeros com comentarios desnecessarios

---
+++ MicrosoftLearn/csharp-readable-code/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+//Print out each order ID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
