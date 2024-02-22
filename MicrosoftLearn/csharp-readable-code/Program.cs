f0a83ef0abbfbccc2301d90326e34e6bf453cdfe
Leonardo <leonardomonteirolima@gmail.com>
2024-02-22 19:47:59 -0300

    Inserido um comentário conciso

--- MicrosoftLearn/csharp-readable-code/Program.cs
+++ MicrosoftLearn/csharp-readable-code/Program.cs
@@ -4,6 +4,14 @@ int widgetsPurchased = 7;
 Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
 */
 
+
+/*
+  O código a seguir cria cinco OrderIDs aleatórios
+  para testar o processo de detecção de fraude. IDs do pedido
+  consistem em uma letra de A a E e três
+  digite o número. Ex. A123.
+*/
+
 Random random = new Random();
 string[] orderIDs = new string[5];
 
@@ -12,6 +20,7 @@ for (int i = 0; i < orderIDs.Length; i++)
     int prefixValue = random.Next(65, 70);
     string prefix = Convert.ToChar(prefixValue).ToString();
     string suffix = random.Next(1, 1000).ToString("000");
+
     orderIDs[i] = prefix + suffix;
 }
 
