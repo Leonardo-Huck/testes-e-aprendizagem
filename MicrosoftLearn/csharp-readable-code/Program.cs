fe4738388d88b3af3d1a8689a71765a7fb618a82
Leonardo <leonardomonteirolima@gmail.com>
2024-02-22 20:40:54 -0300

    Melhorado a legibilidade do código

--- MicrosoftLearn/csharp-readable-code/Program.cs
+++ MicrosoftLearn/csharp-readable-code/Program.cs
@@ -69,16 +69,25 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 Console.WriteLine("\n--------Desafio-------");
 Console.WriteLine("-----------------------\n");
 
-string str = "The quick brown fox jumps over the lazy dog.";
-// convert the message into a char array
-char[] charMessage = str.ToCharArray();
-// Reverse the chars
+/*
+A finalidade desse código é inverter uma cadeia de caracteres
+e contar o número de vezes que um caractere específico é exibido.
+*/
+
+string message = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = message.ToCharArray();
 Array.Reverse(charMessage);
-int x = 0;
-// count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
-// convert it back to a string
-string new_message = new String(charMessage);
-// print it out
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+
+int letterCount = 0;
+foreach (char letter in charMessage)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+string newMessage = new String(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
