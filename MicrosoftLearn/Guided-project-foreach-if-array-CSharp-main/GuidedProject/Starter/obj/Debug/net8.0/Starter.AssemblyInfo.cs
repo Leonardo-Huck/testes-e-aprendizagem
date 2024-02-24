18b3aacdd49ef6711b9ba9c7ffac467acc5c5aac
Leonardo <leonardomonteirolima@gmail.com>
2024-02-23 21:07:02 -0300

    Modificado o codigo para incluir pontos extras do aluno

--- MicrosoftLearn/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ MicrosoftLearn/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+623f3200ab84b8a80765101d41404bb542be881c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27c6373a6822c70731d3e619f4680d831900eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
